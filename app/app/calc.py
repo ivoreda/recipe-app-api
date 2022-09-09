2f3e4cecb19204a2cee17bc13ea7339fc1a1865a
Ivor Edafe <ivor@Ivors-MacBook-Air.local>
2022-09-09 18:18:33 +0100

    Added wait_for_db command

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """add x and y and return result"""
+    return x + y
+
+def subtract(x,y):
+    """subtract x from y and return result"""
+    return y - x
