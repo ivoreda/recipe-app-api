aae7bd205022016040ea557d5c9981ca3f435ba1
Ivor Edafe <ivor@Ivors-MacBook-Air.local>
2022-09-10 09:36:00 +0100

    Fixed Rest framework spelling error

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
     'rest_framework',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REFT_FRAMWORK = {
+REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
